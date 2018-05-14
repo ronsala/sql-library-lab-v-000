18a8610e7c87f9693f82604a216c3004208c4473
Ron Sala <ron.sala@gmail.com>
2018-05-14 12:07:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT series.title, COUNT(characters.species)
   FROM series
   JOIN characters
   ON series.id = characters.series_id
