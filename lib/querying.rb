3c61f40eab5fc4145b374a0b252a6521ae4c98da
Ron Sala <ron.sala@gmail.com>
2018-05-14 11:46:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,11 +14,16 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
+  # "SELECT characters.species, COUNT(characters.species)
+  # AS most_prolific
+  # FROM characters
+  # GROUP BY characters.species
+  # ORDER BY most_prolific DESC
+  # LIMIT 1;"
   "SELECT characters.species, COUNT(characters.species)
-  AS most_prolific
   FROM characters
   GROUP BY characters.species
-  ORDER BY most_prolific DESC
+  ORDER BY COUNT(characters.species) DESC
   LIMIT 1;"
 end
 
