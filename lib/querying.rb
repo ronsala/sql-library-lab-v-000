aea70211a06742c6ccb5c469771b791e398583d1
Ron Sala <ron.sala@gmail.com>
2018-05-14 11:42:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,12 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  AS most_prolific
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY most_prolific DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
