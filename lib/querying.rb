f87107d99f3bab4cd9a7a02dc6c6d0cf95522555
Ron Sala <ron.sala@gmail.com>
2018-05-14 12:43:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books)
+  FROM characters
+  JOIN series
+  ON characters.series_id = series.id
+  "
 end
